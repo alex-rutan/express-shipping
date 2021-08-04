58e9a959364b891c346ed9fdbc93989ecf7d44a5
jragni <jagni001@ucr.edu>
2021-08-03 18:05:48 -0700

    FEAT added axisMOCK test

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,27 @@
 "use strict";
-
-const {
-  shipProduct,
-} = require("./shipItApi");
-
+const axios = require("axios");
+const axiosMockAdapter = require("axios-mock-adapter");
+const axiosMock = new axiosMockAdapter(axios);
+const { shipProduct, SHIPIT_SHIP_URL } = require("./shipItApi");
 
 test("shipProduct", async function () {
+
+  axiosMock.onPost(`${SHIPIT_SHIP_URL}`)
+          .reply(200, {
+            receipt: { 
+                       itemId:1001, 
+                       name: "Test Tester",
+                       addr:"100 Test St", 
+                       zip:"12345-6789", 
+                       shipId:1,
+                     }
+          });
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
     addr: "100 Test St",
     zip: "12345-6789",
   });
-
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(1);
 });
